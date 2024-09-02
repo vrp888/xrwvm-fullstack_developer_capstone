8ba791d3be2730662ada6e8a8bd73d14ad4971f5
Vorapol Rinsatitnon <vorapol.r@outlook.com>
2024-09-02 04:06:45 -0400

    Implement dealer review

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -117,6 +117,13 @@ def get_dealer_details(request, dealer_id):
     else:
         return JsonResponse({"status":400,"message":"Bad Request"})
 
-# Create a `add_review` view to submit a review
-# def add_review(request):
-# ...
+def add_review(request):
+    if(request.user.is_anonymous == False):
+        data = json.loads(request.body)
+        try:
+            response = post_review(data)
+            return JsonResponse({"status":200})
+        except:
+            return JsonResponse({"status":401,"message":"Error in posting review"})
+    else:
+        return JsonResponse({"status":403,"message":"Unauthorized"})
