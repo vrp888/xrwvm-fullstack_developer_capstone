912e8aa4c2af8373e4e943a36feceffad1386e81
Vorapol Rinsatitnon <vorapol.r@outlook.com>
2024-09-02 19:12:07 +1000

    Remove endpoints

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -29,10 +29,9 @@ SECRET_KEY =\
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'localhost',
-    'https://vorapolr-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
-CSRF_TRUSTED_ORIGINS = [
-    'https://vorapolr-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+    'localhost'
+]
+CSRF_TRUSTED_ORIGINS = []
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
