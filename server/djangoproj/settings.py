d9624b68c5fd4b8c649755f7415e29295e2e6914
Vorapol Rinsatitnon <vorapol.r@outlook.com>
2024-09-02 19:43:32 +1000

    Re-add endpoint

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -29,9 +29,12 @@ SECRET_KEY =\
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'localhost'
-]
-CSRF_TRUSTED_ORIGINS = []
+    'localhost',
+    'https://vorapolr-8000.theiadockernext-1-labs'
+    '-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+CSRF_TRUSTED_ORIGINS = [
+    'https://vorapolr-8000.theiadockernext-1-labs'
+    '-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
